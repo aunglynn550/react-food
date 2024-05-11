cb6801d1c962049da43ad872bf454a5ccc20f96b
aunglynn550 <aunglynn550@gmail.com>
2024-05-11 13:28:25 +0630

    init

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,5 @@
+import girldev_8 from './girldev-8.jpg'
+
+export const assets = {
+    girldev_8
+}
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import './Navbar.css';
+import { assets } from '../../assets/assets';
+
+const Navbar = () => {
+    const[menu, setMenu] = useState("home")
+  return (
+    <div className='navbar'>
+        <img className='logo' src={assets.girldev_8} alt="" />
+        <ul className='navbar-menu'>
+            <li onClick={()=>setMenu("home")} className={menu==="home"? "active": ""}>home</li>
+            <li onClick={()=>setMenu("menu")} className={menu==="menu"? "active": ""}>menu</li>
+            <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"? "active": ""}>mobile app</li>
+            <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"? "active": ""}>contact us</li>
+        </ul>
+        <div className='navbar-right'>
+            <img className='icon' src={assets.girldev_8} alt="" />
+            <div className='navbar-search-icon'>
+                <img className='icon' src={assets.girldev_8} alt="" />
+                <div className='dot'>
+
+                </div>
+            </div>
+                <button className='navbar-button'>sign in</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
