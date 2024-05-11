8ebfcbe523882de4f9a2344040e15227b608bf82
aunglynn550 <aunglynn550@gmail.com>
2024-05-11 13:47:24 +0630

    setting up routes for pages

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header-contents'>
+        <h2>Order your favourite Food Here!</h2>
+        <p>Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit. Maiores molestiae enim molestias
+        totam, sunt eaque sequi. Quia, molestiae magnam tenetur 
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
