0eb8d0204334c28254617da0061dd7d37575fca3
aunglynn550 <aunglynn550@gmail.com>
2024-05-16 11:57:45 +0630

    styling Explore Menu

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,5 +1,78 @@
 import girldev_8 from './girldev-8.jpg'
+import girldev_10 from './girldev-10.jpg'
+import girldev_11 from './girldev-11.jpg'
+import girldev_13 from './girldev-13.jpg'
+import gnl_2 from './gnl-2.jpg'
+import gnl_3 from './gnl-3.jpg'
+import gnl_4 from './gnl-4.jpg'
+import gnl_5 from './gnl-5.jpg'
+import gnl_6 from './gnl-6.jpg'
+import gnl_7 from './gnl-7.jpg'
+import gnl_8 from './gnl-8.jpg'
+import gnl_9 from './gnl-9.jpg'
+import gnl_10 from './gnl-10.jpg'
+import gnl_11 from './gnl-11.jpg'
+import gnl_12 from './gnl-12.jpg'
 
 export const assets = {
     girldev_8
 }
+
+export const menu_list = [
+    {
+        menu_name : "Salad",
+        menu_image : girldev_10,
+    },
+    {
+        menu_name : "Rolls",
+        menu_image : girldev_11,
+    },
+    {
+        menu_name : "Deserts",
+        menu_image : girldev_13,
+    },
+    {
+        menu_name : "Sandwich",
+        menu_image : gnl_2,
+    },
+    {
+        menu_name : "Coke",
+        menu_image : gnl_3,
+    },
+    {
+        menu_name : "Pure Veg",
+        menu_image : gnl_4,
+    },
+    {
+        menu_name : "Pasta",
+        menu_image : gnl_5,
+    },
+    {
+        menu_name : "Noodles",
+        menu_image : gnl_6,
+    },
+    {
+        menu_name : "Pepsi",
+        menu_image : gnl_7,
+    },
+    {
+        menu_name : "Paper",
+        menu_image : gnl_8,
+    },
+    {
+        menu_name : "Smooth",
+        menu_image : gnl_9,
+    },
+    {
+        menu_name : "KFC",
+        menu_image : gnl_10,
+    },
+    {
+        menu_name : "Madona",
+        menu_image : gnl_11,
+    },
+    {
+        menu_name : "Cherry Chang",
+        menu_image : gnl_12,
+    }
+]
