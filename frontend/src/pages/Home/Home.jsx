fb2f1d3f02ee47b9610b9a6fae374fd1b1ffd973
aunglynn550 <aunglynn550@gmail.com>
2024-05-22 09:04:32 +0630

    working with category state change

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 const Home = () => {
+  const [category, setCategory] = useState("All")
   return (
     <div>
       <Header/>
-      <ExploreMenu/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }
