cd47c2ace07a33e920e5d8935331e2e8e390c6cd
aunglynn550 <aunglynn550@gmail.com>
2024-05-24 10:09:32 +0630

    setting up AppDownload section

---
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './AppDownload.css'
+const AppDownload = () => {
+  return (
+    <div className='app-download' id='app-download'>
+        <p>For Better Experience Download <br />DeliveryRk App</p>
+        <div className="app-download-platforms">
+            <img src="" alt="" />
+            <img src="" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default AppDownload
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload'
 const Home = () => {
   const [category, setCategory] = useState("All")
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
       <FoodDisplay category={category}/>
+      <AppDownload/>
     </div>
   )
 }
