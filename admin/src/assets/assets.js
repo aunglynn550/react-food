3f0f383b51fc420951bfeb72f2f3361abb69377e
aunglynn550 <aunglynn550@gmail.com>
2024-05-27 10:47:59 +0630

    setting up sidebar&navbar

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,11 @@
+import add_icon from './add_icon.png'
+import logo from './logo.jpg'
+import profile from './profile.jpg'
+
+
+export const assets = {
+    profile,
+    add_icon,  
+    logo,
+  
+ }
